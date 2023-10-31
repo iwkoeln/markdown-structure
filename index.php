03c4e2382253a919bc818e5b6dba4f019a5f0e07
Joel Mai <mai@iwkoeln.de>
2023-10-31 17:23:10 +0100

    [TASK] Updated example
    Tasks:
    * Refactored example file index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use Iwm\MarkdownStructure\MarkdownProjectFactory;
+use Iwm\MarkdownStructure\Parser\CombineTextAndImagesParser;
+use Iwm\MarkdownStructure\Parser\CombineTextAndListParser;
+use Iwm\MarkdownStructure\Parser\HeadlinesToSectionParser;
+use Iwm\MarkdownStructure\Parser\MarkdownToHTMLParser;
+use Iwm\MarkdownStructure\Parser\ParagraphToContainerParser;
+use Iwm\MarkdownStructure\Parser\RemoveDevSections;
+use Iwm\MarkdownStructure\Parser\SectionsToHtmlParser;
+use Iwm\MarkdownStructure\Parser\SplitByEmptyLineParser;
+use Iwm\MarkdownStructure\Utility\FilesFinder;
+use League\CommonMark\Exception\CommonMarkException;
+use League\Config\Exception\ConfigurationExceptionInterface;
+use Symfony\Component\ErrorHandler\Debug;
+
+Debug::enable();
+
+$basePath = '/var/www/html';
+$mdProjectPath = '/var/www/html/tests/Data';
+$indexPath = '/var/www/html/tests/Data/index.md';
+$url = 'https://bitbucket.org/iwm/markdown-structure/src/master/';
+
+$factory = new MarkdownProjectFactory($basePath, $url, $mdProjectPath, $indexPath);
+
+$factory->addValidators([
+    new \Iwm\MarkdownStructure\Validator\MarkdownProjectValidator(),
+    new \Iwm\MarkdownStructure\Validator\MediaFileValidator(),
+]);
+
+$factory->addFileParsers([
+    new SplitByEmptyLineParser(),
+    new HeadlinesToSectionParser(),
+    new RemoveDevSections(),
+    new CombineTextAndImagesParser(),
+    new CombineTextAndListParser(),
+    new MarkdownToHTMLParser(),
+    new ParagraphToContainerParser(),
+    new SectionsToHtmlParser()
+]);
+
+$project = $factory->create();
+dump($project);
+
+//$factory->loadFilesByPath('/var/www/html/local_packages/general-editors-guide');
+//$factory->addExternalSource('baum', '/var/www/html/vendor');
+//$factory->enableValidation = false;
+//$factory->enableNestedStructure = false;
+/*$factory->setValidators([
+    new ImageValidator(),
+    new LinkValidator(),
+    new MarkdownValidator(),
+]);
+
+$factory->setErrorHandlers([
+    new ImageErrorHandler(),
+    new LinkErrorHandler(),
+    new MarkdownErrorHandler(),
+]);*/
+
